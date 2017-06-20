3fb261d3258ce1a18ec72b8aadb20d59026cc0b9
Feng Tian <ftian@vitessedata.com>
2017-06-20 18:03:39 +0000

    Use ssh for git clone.

--- bootstrap-centos.sh
+++ bootstrap-centos.sh
@@ -1,4 +1,8 @@
-git clone git@github.com:vitesse-ftian/gpdb.git
-git clone git@github.com:vitesse-ftian/gporca.git
-git clone git@github.com:vitesse-ftian/incubator-madlib.git
-git clone git@github.com:vitesse-ftian/postgis.git 
+sudo yum install -y unzip which tar more git vim util-linux-ng
+sudo yum install -y net-tools iproute ncurses ncurses-devel
+sudo yum groupinstall -y "Development Tools"
+
+git clone https://github.com/vitesse-ftian/gpdb.git
+git clone https://github.com/vitesse-ftian/gporca.git
+git clone https://github.com/vitesse-ftian/incubator-madlib.git
+git clone https://github.com/vitesse-ftian/postgis.git
