6912b72aba00c66752a57b4b73039e99b799c1b5
Feng Tian <ftian@vitessedata.com>
2017-06-28 17:38:05 +0000

    Ubuntu build.

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,3 +1,19 @@
-git clone git@github.com:vitesse-ftian/gpdb.git
-git clone git@github.com:vitesse-ftian/gporca.git
-git clone git@github.com:vitesse-ftian/incubator-madlib.git
+sudo apt-get update
+sudo apt-get install -y git vim tmux openssh-client openssh-server sudo
+sudo apt-get install -y flex bison autoconf libtool bzip2
+sudo apt-get install -y libyaml-dev libreadline-dev zlib1g-dev libssl-dev
+sudo apt-get install -y python-dev libpython-dev
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libevent-dev libxml2 libxml2-dev libbz2-dev
+sudo apt-get install -y libapr1-dev
+sudo apt-get install -y python-pip
+sudo pip install --upgrade pip
+sudo pip install setuptools
+sudo pip install psutil
+sudo pip install lockfile
+sudo pip install paramiko
+
+git clone https://github.com/vitesse-ftian/gpdb.git
+git clone https://github.com/vitesse-ftian/gporca.git
+git clone https://github.com/vitesse-ftian/gp-xerces.git
+git clone https://github.com/vitesse-ftian/incubator-madlib.git
