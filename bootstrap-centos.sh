f1d3e69acf79bf5003bb67f5508406947e17fe61
Feng Tian <ftian@vitessedata.com>
2018-02-01 21:19:28 -0800

    Update bootstrap-centos.sh

--- bootstrap-centos.sh
+++ bootstrap-centos.sh
@@ -20,7 +20,7 @@ echo '/usr/local/lib' > ./usrlocal.conf
 sudo mv ./usrlocal.conf /etc/ld.so.conf.d/
 sudo ldconfig
 
-git clone https://github.com/vitesse-ftian/gpdb.git 
+git clone https://github.com/greenplum-db/gpdb.git 
 git clone https://github.com/greenplum-db/gporca.git
 git clone https://github.com/greenplum-db/gp-xerces.git
 git clone https://github.com/apache/madlib.git 
